3dc054fc60b0f72f8042ca0fd0c50db0cfb15f39
Omkar4033 <91587106+Omkar4033@users.noreply.github.com>
2024-11-24 19:01:49 +0530

    Navbar-change done by omkar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 
 const App = () => (
   <div className="  bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100 min-h-screen">
-    {/* Navbar with dynamic user role */}
+    
     <Navbar userRole="admin" isLoggedIn={false} />
 
     
